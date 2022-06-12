df4154c6cbd10c9385c27858cf7648fb124fe8a0
LCAndy <loopcreativeandy@gmail.com>
2022-06-12 23:40:40 +0200

    another test

--- tests/dvp.ts
+++ tests/dvp.ts
@@ -23,17 +23,15 @@ describe('dvp', () => {
   const program = anchor.workspace.Dvp as Program<Dvp>;
 
   const testUserKP = loadWalletKey("./TSTvzYALftrptX4V2sVKVn9EU6ogtwbdPnpc63w21s5.json")
+  const uidstring = "16358092002033786";
+  const uid = new anchor.BN(uidstring);
+  const uname = "Andy#007";
+  const seed1 = Buffer.from(anchor.utils.bytes.utf8.encode("discord"));
+  const seed2 = uid.toArray("le",8);
+  const [userAccount, _bump] = findProgramAddressSync([seed1, seed2], program.programId);
 
 
   it('Is signed up!', async () => {
-    const uid = new anchor.BN("16358092002033784");
-    const uname = "Andy#007";
-    const seed1 = Buffer.from(anchor.utils.bytes.utf8.encode("discord"));
-    const seed2 = uid.toArray("le",8);
-    console.log(seed2);
-    const [userAccount, _bump] = findProgramAddressSync([seed1, seed2], program.programId);
-    console.log(userAccount);
-    console.log(program.programId.toBase58());
     const tx = await program.rpc.singup(uid, uname, 
     {
       accounts: {
@@ -45,4 +43,21 @@ describe('dvp', () => {
     });
     console.log("Your transaction signature", tx);
   });
+
+  it('verify!', async () => {
+    
+    const ua = await anchor.getProvider().connection.getAccountInfo(userAccount);
+    assert(ua.owner.toBase58() === program.programId.toBase58());
+    assert(ua.data.at(8) === 1, "state should be 1");
+    console.log(ua.data.readBigUInt64LE(8+1+32));
+    assert(ua.data.readBigUInt64LE(8+1+32) === BigInt(uidstring), "user id not set correctly");
+    const wallet = new anchor.web3.PublicKey(ua.data.slice(8+1, 8+1+32));
+    console.log(wallet.toBase58());
+    assert(wallet.toBase58() === testUserKP.publicKey.toBase58(), "user PK not set correctly");
+  });
 });
+
+function assert(condition: boolean, msg = "assertion failed") {
+  if(!condition)
+    throw new Error(msg);
+}
