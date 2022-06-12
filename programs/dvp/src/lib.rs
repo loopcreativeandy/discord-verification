fce0ba34daccfa01edf67d6c9f4c9d3c2c395510
LCAndy <loopcreativeandy@gmail.com>
2022-06-12 22:06:34 +0200

    attempt to use seed

--- programs/dvp/src/lib.rs
+++ programs/dvp/src/lib.rs
@@ -1,21 +1,21 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("DVPyjRxvx7ABzpnFCrvdDB2kLd1FDQBkRzfrLpHtnNvk");
 
 #[program]
 pub mod dvp {
     use super::*;
-    pub fn singup(ctx: Context<SignUpAccounts>, dc_id: u64, dc_name: String) -> ProgramResult {
+    pub fn singup(ctx: Context<SignUpAccounts>, dc_id: u64, dc_name: String) -> Result<()> {
         let user_account = &mut ctx.accounts.user_account;
-        if name.as_bytes().len() > UserAccount::MAX_NAME_LENGH {
-            msg!("User Name too long! ({} characters max)",UserAccount::MAX_NAME_LENGH)
+        if dc_name.as_bytes().len() > UserAccount::MAX_NAME_LENGH {
+            msg!("User Name too long! ({} characters max)",UserAccount::MAX_NAME_LENGH);
             panic!();
         }
+        msg!("Signing up {} ({})",dc_name,dc_id);
         user_account.state = 1;
         user_account.wallet = ctx.accounts.signer.key();
         user_account.discord_id = dc_id;
         user_account.discord_name = dc_name;
-        msg!("Signed up {} ({})",dc_name,dc_id)
         Ok(())
     }
 }
@@ -30,15 +30,17 @@ pub struct UserAccount {
 }
 
 impl UserAccount {
-    pub const MAX_NAME_LENGH: usize: 37;
-    pub const MAX_SIZE: usize = 1 + 32 + 64 + (4 + MAX_NAME_LENGH);
+    pub const MAX_NAME_LENGH: usize = 37;
+    pub const MAX_SIZE: usize = 1 + 32 + 64 + (4 + UserAccount::MAX_NAME_LENGH);
 }
 
 #[derive(Accounts)]
+#[instruction(dc_id: u64)]
 pub struct SignUpAccounts<'info> {
     #[account(init, payer = signer, space = 8 + UserAccount::MAX_SIZE,
-        seeds = [b"discord", signer.key().as_ref()], bump)]
+        seeds = [b"discord".as_ref(), &dc_id.to_le_bytes()], bump)]
     pub user_account: Account<'info, UserAccount>,
+    #[account(mut)]
     pub signer: Signer<'info>,
     pub system_program: Program<'info, System>
 }
--- tests/dvp.ts
+++ tests/dvp.ts
@@ -1,6 +1,19 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
+import { findProgramAddressSync } from '@project-serum/anchor/dist/cjs/utils/pubkey';
 import { Dvp } from '../target/types/dvp';
+
+export function loadWalletKey(keypairFile:string): anchor.web3.Keypair {
+  if (!keypairFile || keypairFile == '') {
+    throw new Error('Keypair is required!');
+  }
+  const fs = require("fs");
+  const loaded = anchor.web3.Keypair.fromSecretKey(
+    new Uint8Array(JSON.parse(fs.readFileSync(keypairFile).toString())),
+  );
+  console.log(`using wallet: ${loaded.publicKey}`);
+  return loaded;
+}
 
 describe('dvp', () => {
 
@@ -9,9 +22,23 @@ describe('dvp', () => {
 
   const program = anchor.workspace.Dvp as Program<Dvp>;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
+  const testUserKP = loadWalletKey("./TSTvzYALftrptX4V2sVKVn9EU6ogtwbdPnpc63w21s5.json")
+
+
+  it('Is signed up!', async () => {
+    const userID = 16358092002033784;
+    const seed1 = Buffer.from(anchor.utils.bytes.utf8.encode("discord"));
+    //const seed2 = Buffer.from(anchor.utils.bytes.utf8.encode("discord"));
+    const [userAccount, _bump] = findProgramAddressSync([seed1], program.programId);
+    console.log(userAccount)
+    const tx = await program.rpc.singup({
+      accounts: {
+        userAccount,
+        signer: testUserKP.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [testUserKP],
+    });
     console.log("Your transaction signature", tx);
   });
 });
