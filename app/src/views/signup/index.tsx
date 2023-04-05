6b3336c813bcd116db92cc32521f6e36f7b31fce
Solandy <solandy.consulting@gmail.com>
2023-04-05 17:26:14 +0200

    fix more special characters

--- app/src/views/signup/index.tsx
+++ app/src/views/signup/index.tsx
@@ -8,7 +8,7 @@ export const SignupView: FC = ({ }) => {
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mt-10 mb-8">
-          Sign up for Andy's Discord
+          Sign up
         </h1>
         {/* CONTENT GOES HERE */}
         <div className="text-center">
